cc16af175121e57bd416c68eb9c9b6f98b5b42ac
Graham <graham.t.holt@gmail.com>
2018-07-18 16:44:56 -0400

    script is now functional

--- bin/test_gen.py
+++ bin/test_gen.py
@@ -39,24 +39,42 @@ def test_gen(pdb):
     #interface.interface()
     # Note - this command seems to work well for picking only
     # sidechain-sidechain contacts
-    interface.interface("","",3,1)
+    interface.interface(None,None,3,1)
     try:
-        inter_list = cmd.get_names("selections", all)
-        print inter_list
+        inter_list = cmd.get_names("selections", 0, "all")
     except:
         raise CmdException
-        inter_list = [ e for e in inter_list if e[0:2]=="int" ]
+
+    inter_list = [ e for e in inter_list if e[0:3]=="int" ]
 
     # Find potential mutable residues
+    mut_list = []
     for inter in inter_list:
-        mut_list.append(*gen_mut(inter))
+        chains = cmd.get_chains(inter)
+        inter_1 = inter+' and chain '+chains[0]
+        inter_2 = inter+' and chain '+chains[1]
+        print inter_1
+        mut_list.extend(gen_mut(inter_1))
+        print inter_2
+        mut_list.extend(gen_mut(inter_2))
+
+    gen_scenes(mut_list)
 
     # For each mutable residue set, generate a flexible shell and print design
     for mut in mut_list:
         #Select muts
-        #gen_shell(1)
+        obj_name = cmd.get_names('objects',0,'all')[0]
+        resi_list = [ str(res.res_seq)+res.i_code for res in mut ]
+        chain = next(iter(mut)).chain_id
+        selection = "br. chain "+chain+" and resi "+'+'.join(resi_list)
+        cmd.select("mut", selection)
+
+        #gen_shell
+        gen_shell.gen_shell(1,0,"mut",5)
+
         #design()
-        pass
+        design.design("mut","flex",('_'.join([obj_name, chain,
+                                              "confsize"]))+".cfs")
 
 
 def gen_mut(sele_name):
@@ -69,18 +87,66 @@ def gen_mut(sele_name):
         sele_name (string): The name of a PyMol selection that defines a PPI
 
     Returns:
-        A list of lists of Residue objects that are good design choices
+        A set of sets of Residue objects that are good design choices
     """
-    mut_list = []
+    mut_list = set()
     res_list = residue.Residue.sele_to_res(sele_name)
 
     # Add all single mutations
-    mut_list.append(*[ [res] for res in res_list ])
+    mut_list.update(frozenset([res]) for res in res_list)
 
-    # Construct basis for close pairs
-    close_pair_basis = [ e for e in itertools.combinations(mut_list, 2)
-                        if e[0].min_sc_sc_distance(e[1]) < 3 ]
-    print close_pair_basis
+    # Construct close pairs
+    close_pairs = set( frozenset(e) for e in itertools.combinations(
+        [ res for res in res_list ], 2)
+                        if e[0].min_sc_sc_distance(e[1]) < 3 )
+
+    # Add close pairs to muts
+    mut_list.update(close_pairs)
+
+    # Iteratively union close pairs until cannot any longer
+    new_union = True
+    old_set = close_pairs
+    while new_union == True:
+        new_union = False
+        new_mut_set = set()
+        for e in old_set:
+            for pair in close_pairs:
+                if len(e.intersection(pair)) == 1:
+                    putative_design = e.union(pair)
+                    if is_globular(putative_design, 3):
+                        new_mut_set.add(putative_design)
+                        new_union = True
+        mut_list.update(new_mut_set)
+        old_set = new_mut_set
+
+    for mut in mut_list:
+        print ', '.join(str(e) for e in mut)
+    return mut_list
+
+def is_globular(mut_set, dist):
+    max_dist = 0
+    for pair in itertools.combinations(iter(mut_set), 2):
+        if pair[0].max_sc_sc_distance(pair[1]) < dist:
+            return False
+
+    return True
+
+def gen_scenes(mut_list):
+    # generate a reference
+    cmd.create("reference", "all", 1, 1)
+
+    # create scenes for each mut
+    for index, mut in enumerate(mut_list):
+        obj_name = cmd.get_names('objects',0,'all')[0]
+
+    # modify individual states
+        resi_list = [ str(res.res_seq)+res.i_code for res in mut ]
+        chain = next(iter(mut)).chain_id
+        selection = "br. chain "+chain+" and resi "+'+'.join(resi_list)
+        cmd.create(obj_name, selection, 1, index+2)
+
+        cmd.set("stick_color","red", "all", state=index)
+        cmd.show("sticks",selection)
 
 # Make this runnable as a command from pymol
 cmd.extend( "test_gen", test_gen )
